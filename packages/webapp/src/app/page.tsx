f9551970de029d53bc35d566a7d2fc0758022510
Konstantin Fastov <kfastov@gmail.com>
2024-12-18 16:28:40 +0700

    refactor: use environment variable for webapp URL
    - Replaced hardcoded Telegram bot URL with `WEBAPP_URL` environment variable in `page.tsx`.
    - Updated `next.config.ts` to load environment variables from `.env.local` located in the root directory.
    - Added `.env.example` to provide example configuration for environment variables.
    - Changed text in `page.tsx` to refer to the webapp URL.

--- packages/webapp/src/app/page.tsx
+++ packages/webapp/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 export default function Home() {
+  const webappUrl = process.env.WEBAPP_URL || 'https://t.me/StarBeamBot/wallet';
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -8,8 +9,8 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center">Welcome to Starbeam</h1>
         
         <div className="flex flex-col gap-4 items-center">
-          <Link href={'https://t.me/StarBeamBot/wallet'} className="px-4 py-2 bg-white rounded-md text-black">
-            Open Mini-App in Telegram
+          <Link href={webappUrl} className="px-4 py-2 bg-white rounded-md text-black">
+            Open Webapp in Telegram
           </Link>
         </div>
       </main>
