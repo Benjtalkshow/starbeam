dfcb82866faafa91f4789b1bf68f01a30679f088
Shantel peters. <Elizabethogunrinde2015@gmail.com>
2024-12-20 02:12:51 +0000

    Add BiometricAuth component and integrate with page.tsx

--- packages/webapp/src/app/page.tsx
+++ packages/webapp/src/app/page.tsx
@@ -1,9 +1,27 @@
-import BiometricAuth from './components/BiometricAuth';
+import { useLaunchParams, biometry } from "@telegram-apps/sdk-react";
+import { useEffect, useState } from "react";
+import TelegramAuth from '@/app/components/TelegramAuth';
 
-export default function Home() {
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const launchParams = useLaunchParams();
+
+  useEffect(() => {
+    const init = async () => {
+      setIsLoading(false);
+    };
+
+    init();
+  }, []);
+
+  if (isLoading) {
+    return <div className="p-4">Loading...</div>;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <BiometricAuth />
+      <TelegramAuth />
+      {/* Add other components/content here */}
     </main>
   );
 }
